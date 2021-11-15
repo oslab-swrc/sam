8a72cfcdc34cb23dcb4599f96b67c0c5ed720501
yeoneo <yeoneo@pusan.ac.kr>
2021-11-16 08:49:30 +0900

    reset

--- sam-stm/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Revision history for sam-stm
-
-## 0.1.0.0 -- YYYY-mm-dd
-
-* First version. Released on an unsuspecting world.
--- sam-stm/Setup.hs
+++
@@ -1,7 +0,0 @@
-{-
-SPDX-FileCopyrightText: Copyright (c) 2021 Pusan National University
-
-SPDX-License-Identifier: BSD-3 License
--}
-import Distribution.Simple
-main = defaultMain
